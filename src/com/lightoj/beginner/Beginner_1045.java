391a6c2f53482580742c5edf5ecd53aa19d8ee97
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-10-18 16:56:04 +0600

    lightoj - 1045

---
+++ src/com/lightoj/beginner/Beginner_1045.java
@@ -0,0 +1,145 @@
+package com.lightoj.beginner;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.io.PrintWriter;
+
+class FasterScanner {
+    private InputStream mIs;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    public FasterScanner() {
+        this(System.in);
+    }
+
+    public FasterScanner(InputStream is) {
+        mIs = is;
+    }
+
+    public int read() {
+        if (numChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = mIs.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public String nextLine() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    public String nextString() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    public long nextLong() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    public boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+}
+
+public class Beginner_1045 {
+
+    public static void main(String[] args) {
+        FasterScanner fastScanner = new FasterScanner();
+        PrintWriter printWriter = new PrintWriter(System.out);
+
+        double[] sums = new double[1000001];
+        double sum = 0;
+        sums[0] = 0;
+
+        for(int i = 1;i<sums.length;i++) {
+            sum += Math.log10(i);
+            sums[i] = sum;
+        }
+
+        int testCase, number, base, ans;
+        testCase = fastScanner.nextInt();
+
+        for(int i = 0; i < testCase; i++) {
+            number = fastScanner.nextInt();
+            base = fastScanner.nextInt();
+
+            ans = (int) (sums[number]/Math.log10(base));
+            ans++;
+
+            printWriter.print("Case ");
+            printWriter.print(i+1);
+            printWriter.print(": ");
+            printWriter.println(ans);
+        }
+        printWriter.flush();
+        printWriter.close();
+    }
+}
