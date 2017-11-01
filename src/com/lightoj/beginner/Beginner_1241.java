695d3473881b6f5aeba495e86608dbb66efd477d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-11-01 16:33:31 +0600

    lightoj - 1241

---
+++ src/com/lightoj/beginner/Beginner_1241.java
@@ -0,0 +1,169 @@
+package com.lightoj.beginner;
+
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+
+
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Tahsin Rashad
+ */
+public class Beginner_1241 {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+
+            int testCase, totalNumbers, currentSize, tempSize, lieCount;
+            int[] noseSize;
+
+            testCase = in.nextInt();
+            for (int i = 1; i <= testCase; i++) {
+
+                totalNumbers = in.nextInt();
+                lieCount = 0;
+                noseSize = new int[totalNumbers + 1];
+                currentSize = 0;
+
+                for (int j = 0; j < totalNumbers; j++) {
+
+                    noseSize[j] = in.nextInt();
+                    tempSize = noseSize[j];
+
+                    if ((j == 0) && tempSize > 2) {
+
+                        currentSize = noseSize[j];
+                        while (tempSize > 2) {
+                            tempSize -= 5;
+                            lieCount++;
+                        }
+                    } else if ((j > 0) && (currentSize < tempSize)) {
+
+                        while (tempSize > currentSize) {
+                            tempSize -= 5;
+                            lieCount++;
+                        }
+                        currentSize = noseSize[j];
+                    } else {
+                        currentSize = noseSize[j];
+                    }
+                }
+                out.printf("Case %d: %d\n", i, lieCount);
+            }
+            out.flush();
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void printf(String format, Object... objects) {
+            writer.printf(format, objects);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
